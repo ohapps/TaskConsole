858ea1295d6ca8d8e43e1594b90b681d2d7b63d5
Craig <craighausner@gmail.com>
2011-04-12 22:02:15 -0500

    updated test cases

--- tests/models/ProjectsTest.php
+++ tests/models/ProjectsTest.php
@@ -22,7 +22,74 @@ class ProjectsTest extends AbstractTest {
 		$this->assertFalse( $project->isActive() );
 		
 	}
+	
+	
+	public function testIsUserProject(){
 		
+		$project = Doctrine_Core::getTable('Console_Project')->find(1);
+		$this->assertTrue( $project->isUserProject( $this->testUserId ) );
+		
+		$project = Doctrine_Core::getTable('Console_Project')->find(3);
+		$this->assertFalse( $project->isUserProject( $this->testUserId ) );
+		
+	}
+	
+	
+	public function testGetStatus(){
+		
+		$project = Doctrine_Core::getTable('Console_Project')->find(1);
+		$this->assertEquals( $project->getStatus(), 'active' );
+		
+		$project = Doctrine_Core::getTable('Console_Project')->find(2);
+		$this->assertEquals( $project->getStatus(), 'complete' );
+		
+	}
+	
+	
+	public function testGetTaskPendingCount(){
+		
+		$project = Doctrine_Core::getTable('Console_Project')->find(1);
+		$this->assertEquals( $project->getTaskPendingCount(), 1 );
+		
+		$project = Doctrine_Core::getTable('Console_Project')->find(2);
+		$this->assertEquals( $project->getTaskPendingCount(), 1 );
+		
+	}
+	
+
+	public function testGetTaskCompleteCount(){
+		
+		$project = Doctrine_Core::getTable('Console_Project')->find(1);
+		$this->assertEquals( $project->getTaskCompleteCount(), 0 );
+		
+		$project = Doctrine_Core::getTable('Console_Project')->find(2);
+		$this->assertEquals( $project->getTaskCompleteCount(), 1 );		
+		
+	}
+		
+	
+	public function testGetTaskTotal(){
+		
+		$project = Doctrine_Core::getTable('Console_Project')->find(1);
+		$this->assertEquals( $project->getTaskTotal(), 1 );
+		
+		$project = Doctrine_Core::getTable('Console_Project')->find(2);
+		$this->assertEquals( $project->getTaskTotal(), 2 );
+		
+	} 
+	
+	
+	public function testIsComplete(){
+		
+		$project = Doctrine_Core::getTable('Console_Project')->find(1);
+		$this->assertEquals( $project->isComplete(), false );
+		
+		$project = Doctrine_Core::getTable('Console_Project')->find(2);
+		$this->assertEquals( $project->isComplete(), true );
+		
+	}
+	
+	
 }
 
 ?>
