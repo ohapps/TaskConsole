3752d48a27a7402c66fedaa29e9fa807630ad397
Craig Hausner <craighausner@gmail.com>
2017-01-20 22:47:18 -0600

    removed SSL check from login screen

--- application/controllers/UserController.php
+++ application/controllers/UserController.php
@@ -1,180 +1,174 @@
 <?php
 
-class UserController extends Zend_Controller_Action {	
-	
-	
+class UserController extends Zend_Controller_Action {
+
+
 	public function init(){
-		
+
 		$this->_helper->layout->setLayout('plain');
-						
-	}			
-	
-	
+
+	}
+
+
 	public function loginAction(){
-		
+
 		$user = Zend_Registry::get('user');
 		$userManager = Zend_Registry::get('userManager');
-		
-		// CHECK IF USERS IS LOGGED IN		
-		if( $user === false ){ 
-		
-			// CHECK FOR SSL -- RE-ENABLE
-			if(isset($_SERVER["HTTPS"]) === false) {
-				header("Location: https://" . $_SERVER["SERVER_NAME"] . $_SERVER["REQUEST_URI"] );
-				exit();
-			}
-			
+
+		// CHECK IF USERS IS LOGGED IN
+		if( $user === false ){			
+
 			// CHECK FOR MOBILE
 			if( $this->_helper->mobile->isMobile() === true || $this->_getParam('layout') == 'mobile' ){
 
 				$this->_helper->layout()->setLayout('mobile');
 				$this->_helper->viewRenderer->setNoRender();
 				$this->render('mobile-login');
-								
-			}			
-			
-						
+
+			}
+
+
 		}else{
-			
+
 			$this->_redirect( $this->view->serverUrl() . $this->view->url(array('controller'=>'index','action'=>'index')) );
-			
+
 		}
-		
+
 	}
-	
-	
+
+
 	public function logoutAction(){
-				
+
 		$userManager = Zend_Registry::get('userManager');
-		
+
 		if( $userManager->loggedIn() === true ){
 			$userManager->logout();
-			$this->view->user = false;	
+			$this->view->user = false;
 		}
-		
+
 		// CHECK FOR MOBILE
-		if( $this->_helper->mobile->isMobile() === true || $this->_getParam('layout') == 'mobile' ){						
+		if( $this->_helper->mobile->isMobile() === true || $this->_getParam('layout') == 'mobile' ){
 			$this->_redirect("/mobile");
 		}else{
 			$this->_redirect("/");
-		}				
-					
+		}
+
 	}
-	
-	
-	public function authAction(){		
-		
+
+
+	public function authAction(){
+
 		$userManager = Zend_Registry::get('userManager');
-		
+
 		if( $userManager->loggedIn() === false ){
-			$userManager->auth( $this->_getParam("un"), $this->_getParam("ps") );			
-		}			
-		
+			$userManager->auth( $this->_getParam("un"), $this->_getParam("ps") );
+		}
+
 		$auth = $userManager->loggedIn();
-		
-		if( $this->_helper->mobile->isMobile() === true ){ 
-				
+
+		if( $this->_helper->mobile->isMobile() === true ){
+
 				$this->_helper->layout()->disableLayout();
 				$this->_helper->viewRenderer->setNoRender();
 				$this->render('iphone-login');
 
 				if( $auth == true ){
 					$this->_redirect( $this->_getParam('success_url') );
-				}else{										
+				}else{
 					$this->_redirect( "/user/login?failed=true&rtn_url=" . $this->_getParam('success_url') );
 				}
-				
-		}else{			
-				$this->_helper->json->sendJson( array("success" => $auth ) );				
-		}	
-		
+
+		}else{
+				$this->_helper->json->sendJson( array("success" => $auth ) );
+		}
+
 	}
-	
-	
+
+
 	public function statusAction(){
-		
+
 		$userManager = Zend_Registry::get('userManager');
 		$this->_helper->json->sendJson( array("success" => $userManager->loggedIn() ) );
-		
+
 	}
-	
-	
+
+
 	/*
 	public function profileAction(){
-		
-		$user = Zend_Registry::get('user');		
-		
-		// CHECK IF USERS IS LOGGED IN		
-		if( $user != false ){ 
-		
+
+		$user = Zend_Registry::get('user');
+
+		// CHECK IF USERS IS LOGGED IN
+		if( $user != false ){
+
 			// CHECK FOR SSL
 			if(isset($_SERVER["HTTPS"]) === false) {
 				header("Location: https://" . $_SERVER["SERVER_NAME"] . $_SERVER["REQUEST_URI"] );
 				exit();
-			}						
-			
-			$this->view->user = $user;			
-			
+			}
+
+			$this->view->user = $user;
+
 		}else{
-			
-			$this->_redirect("/");			
-			
+
+			$this->_redirect("/");
+
 		}
-		
+
 	}
-	
-	
+
+
 	public function saveProfileAction(){
-		
+
 		$user = Zend_Registry::get('user');
-		$userManager = Zend_Registry::get('userManager');				
-		$errors = array();					
-		
-		if( $user != false ){ 
-		
+		$userManager = Zend_Registry::get('userManager');
+		$errors = array();
+
+		if( $user != false ){
+
 			// VALIDATE USERNAME
 			if( $user->setUsername( $this->_getParam('username') ) === false ){
 				$errors["username"] = "The username must be between 4 and 12 characters long and contain only alpha numeric characters";
 			}else if( $userManager->checkUsername( $user ) == false ){
 				$errors["username"] = "The username entered is not available";
-			}			
-			
+			}
+
 			// UPDATE PASSWORD
 			if( $this->_getParam('password') != "" && $this->_getParam('password_check') != "" ){
-					
-				// VALIDATE PASSWORD						
+
+				// VALIDATE PASSWORD
 				if( $this->_getParam('password') != $this->_getParam('password_check') ){
-					$errors["password"] = "The passwords entered do not match";			
+					$errors["password"] = "The passwords entered do not match";
 					$errors["password_check"] = "The passwords entered do not match";
 				}else if( $user->setPassword( $this->_getParam('password') ) === false ){
 					$errors["password"] = "The passwords must be between 6 and 12 characters long and contain only alpha numeric characters";
-				}					
-				
-			}				
-			
+				}
+
+			}
+
 			$user->setFirstName( $this->_getParam('first_name') );
 			$user->setLastName( $this->_getParam('last_name') );
 			$user->setGdataUsername( $this->_getParam('gdata_user') );
-			$user->setGdataPassword( $this->_getParam('gdata_pass') );					
-			
+			$user->setGdataPassword( $this->_getParam('gdata_pass') );
+
 			if( count($errors) > 0 ){
 				$this->_helper->json->sendJson( array("success" => false, "errors" => $errors ) );
 			}else{
 				$userManager->saveUser($user);
 				$this->_helper->json->sendJson( array("success" => true ) );
 			}
-		
+
 		}else{
-			
-			$this->_redirect("/");			
-			
+
+			$this->_redirect("/");
+
 		}
-		
+
 	}
 	*/
-	
-			
-} 
+
+
+}
 
 
 ?>
